94ed815891b27c6243400bb6a991a48220ca2b50
Christian Heinigk <christian.heinigk@nld.rwth-aachen.de>
2023-02-24 19:19:24 +0100

    Add terminal manager table.

---
+++ lua/teaman/init.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+---@class Terminals @contains Terminal objects
+local Terminals = {}
+
+---@brief Add a terminal
+---@param term Terminal @table
+function M.add(term)
+  vim.validate({
+    term = {term, require'teaman.utils'.is_terminal, "Terminal table"},
+  })
+  table.insert(Terminals, term)
+end
+
+---@brief remove a terminal
+---@param term Terminal table
+function M.remove(term)
+  vim.validate({
+    term = {term, require'teaman.utils'.is_terminal, "Terminal table"},
+  })
+  Terminals = vim.tbl_filter(
+    function(t) return t == term end,
+    Terminals
+  )
+end
+
+---@brief list all terminals
+---@return Terminals list of Terminal objects
+function M.list()
+  return Terminals
+end
+
+---@brief filter terminals
+---@param predicate function Predicate function used to filter the terminals
+---@return Terminals list of Terminal objects
+function M.filter(predicate)
+  vim.validate({
+    predicate = {predicate, "function", false},
+  })
+  return vim.tbl_filter(predicate, Terminals)
+end
+
+---@brief apply a function for all terminals
+---@param f function Function acting on a terminal object
+---@return Terminals list of Terminal objects
+function M.map(f)
+  vim.validate({
+    predicate = {f, "function", false},
+  })
+  return vim.tbl_map(f, Terminals)
+end
+
+return M
